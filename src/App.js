e460a22ef31def83a82f2081b50e7ec23f7f9770
Your Name <shafatkashif@gmail.com>
2020-09-28 23:50:26 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Paper, TextField, Button } from "@material-ui/core";
+import NavBar from './NavBar';
+import Prog from './Prog'
+
+
+const useStyles = makeStyles((theme) => ({
+ 
+
+  Paper: {
+    width: 400,
+    margin: "0 auto",
+    marginTop:25,
+    textAlign: "center",
+    padding:15
+  },
+  input: {
+    width: "95%",
+    marginBottom:10
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <NavBar />
+      <Paper elevation={3} className={classes.Paper}>
+        <h1>Login</h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Email" className={classes.input} />
+          <TextField label="Password" className={classes.input} />
+
+          <Button variant="contained" color="primary" className={classes.input}>
+            Primary
+          </Button>
+          <Prog />
+        </form>
+      </Paper>
     </div>
   );
 }
